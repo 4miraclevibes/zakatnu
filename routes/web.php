321cab34b37c56c9ed29482b11c338e35d98666f
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-10-24 23:37:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,42 +7,42 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\Dashboard\DonationController;
 use App\Http\Controllers\Dashboard\TransactionController;
 use Illuminate\Support\Facades\Auth;
-
 Route::get('/', [LandingController::class, 'index'])->middleware('auth')->name('landing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::post('/transactions/{donationId}', [TransactionController::class, 'store'])->middleware('auth')->name('transactions.store');
 
-Route::middleware('auth')->group(function () {
-    Route::middleware(function ($request, $next) {
-        if (Auth::check() && Auth::user()->role_id !== 1) {
-            return redirect('/')->with('error', 'Anda tidak berhak akses halaman ini');
+Route::post('/transactions/{donationId}', [TransactionController::class, 'store'])->middleware('auth')->name('transactions.store');
+Route::group([
+    'middleware' => ['auth', function ($request, $next) {
+        if (Auth::user()->role->name !== 'admin') { // Asumsikan role_id 1 adalah admin
+            return redirect()->route('home')->with('error', 'Anda tidak memiliki akses ke halaman ini.');
         }
         return $next($request);
-    })->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        // Donation
-        Route::get('/donations', [DonationController::class, 'index'])->name('donations.index');
-        Route::post('/donations', [DonationController::class, 'store'])->name('donations.store');
-        Route::get('/donations/{donation}', [DonationController::class, 'show'])->name('donations.show');
-        Route::put('/donations/{donation}', [DonationController::class, 'update'])->name('donations.update');
-        Route::delete('/donations/{donation}', [DonationController::class, 'destroy'])->name('donations.destroy');
-        // User
-        Route::get('/users', [UserController::class, 'index'])->name('users.index');
-        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-        Route::post('/users', [UserController::class, 'store'])->name('users.store');
-        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-        Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-        // Transaction
-        Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
-        Route::put('/transactions/{transaction}', [TransactionController::class, 'update'])->name('transactions.update');
-        Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
-    });
+    }]
+], function () {
+    
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Donation
+    Route::get('/donations', [DonationController::class, 'index'])->name('donations.index');
+    Route::post('/donations', [DonationController::class, 'store'])->name('donations.store');
+    Route::get('/donations/{donation}', [DonationController::class, 'show'])->name('donations.show');
+    Route::put('/donations/{donation}', [DonationController::class, 'update'])->name('donations.update');
+    Route::delete('/donations/{donation}', [DonationController::class, 'destroy'])->name('donations.destroy');
+    // User
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    // Transaction
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::put('/transactions/{transaction}', [TransactionController::class, 'update'])->name('transactions.update');
+    Route::delete('/transactions/{transaction}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
 });
 
 require __DIR__.'/auth.php';
